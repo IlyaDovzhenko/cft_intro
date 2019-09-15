0acebabf32665981c32dfcd7e81ff81644c9bc30
ilya <i.dovzhenko26@gmail.com>
2019-09-15 21:54:33 +0700

    nullPointer

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,20 +4,18 @@ public class Application {
     public static void main(String[] args) {
 
         Parameters parameters = new Parameters(args);
-        System.out.println("Direction: " + parameters.getDirection());
-        System.out.println("Data type: " + parameters.getInputType());
-        System.out.println("Output file: " + parameters.getOutputFile());
-        for (String filePath : parameters.getInputFiles()) {
-            System.out.println("Input files: " + filePath);
-        }
 
         IndexCalculation indexCalculation;
-        if (parameters.getInputType().equals("string")) {
-            indexCalculation = new StringIndexCalculation();
+        try {
+            if (parameters.getInputType().equals("string")) {
+                indexCalculation = new StringIndexCalculation();
+            }
+            else {
+                indexCalculation = new IntegerIndexCalculation();
+            }
+            MergeFiles.merge(parameters, indexCalculation);
+        } catch (NullPointerException ex) {
+            System.out.println("Enter command line parameters!");
         }
-        else {
-            indexCalculation = new IntegerIndexCalculation();
-        }
-        MergeFiles.merge(parameters, indexCalculation);
     }
 }
